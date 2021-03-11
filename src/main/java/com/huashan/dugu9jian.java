fc16af443c0edca17bc88b49c7fb16917c96d2d6
zz <zzkk626521@163.com>
2021-03-11 15:10:39 +0800

    独孤九剑心法

--- src/main/java/com/huashan/dugu9jian.java
+++ src/main/java/com/huashan/dugu9jian.java
@@ -4,5 +4,8 @@ public class dugu9jian {
     public static void main(String[] args) {
 
         System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
     }
 }
